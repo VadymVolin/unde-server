d96e96b61686aa99100927042bfa72984b81627b
Vadym Volin <vadim.volin1@gmail.com>
2025-08-30 22:03:15 +0300

    Socket handler - modified adb manager

--- server/src/main/kotlin/com/unde/server/configuration/adb/AdbManager.kt
+++ server/src/main/kotlin/com/unde/server/configuration/adb/AdbManager.kt
@@ -3,17 +3,12 @@ package com.unde.server.configuration.adb
 import com.unde.server.constants.Time
 import io.ktor.server.application.*
 import io.ktor.util.logging.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import java.io.BufferedReader
-import java.util.*
 
 object AdbManager {
 
-    private var timer: Timer? = null
-
-    private var timerTask: TimerTask? = null
+    private var adbDevicesJob: Job? = null
 
     private val adbErrors = listOf(
         "adb: no devices/emulators found",
@@ -22,42 +17,35 @@ object AdbManager {
     private val LOGGER = KtorSimpleLogger(javaClass.simpleName)
 
     internal fun setup(application: Application) {
-        if (timer == null) {
-            timer = Timer("UndeTimer")
-        }
-        if (timerTask == null) {
-            timerTask = createAdbDevicesTask(application)
-        }
-        timer?.scheduleAtFixedRate(timerTask, Time.INITIAL_ADB_TASK_DELAY, Time.DEFAULT_ADB_TASK_DELAY)
-    }
-
-    internal fun release() {
-        LOGGER.info("Release manager and resources")
-        timer?.cancel()
-        timer = null
-    }
-
-    private fun createAdbDevicesTask(application: Application): TimerTask = object : TimerTask() {
-        override fun run() {
-            val devicesDeferred = application.async(Dispatchers.Default) { getConnectedDevices() }
-            application.launch(Dispatchers.Default) {
-                val devices = devicesDeferred.await()
-                if (devices.isNotEmpty()) {
-                    setupAdbReverse(devices)
+        adbDevicesJob?.cancel()
+        adbDevicesJob = application.launch(Dispatchers.Default) {
+            while (isActive) {
+                val devicesDeferred = application.async(Dispatchers.Default) { getConnectedDevices() }
+                with(devicesDeferred.await()) {
+                    if (isNotEmpty()) {
+                        setupAdbReverse(this)
+                    }
                 }
+                delay(Time.DEFAULT_ADB_TASK_DELAY)
             }
         }
     }
 
+    internal fun release() {
+        LOGGER.info("Release manager and resources")
+        adbDevicesJob?.cancel()
+        adbDevicesJob = null
+    }
+
     private fun getConnectedDevices(): List<String> {
         LOGGER.info("Trying to get connected devices")
-        val process = ProcessBuilder("adb", "devices").redirectErrorStream(true).start()
+        val process = ProcessBuilder("adb", "devices", "-l").redirectErrorStream(true).start()
         process.inputStream.bufferedReader().useLines { lines ->
             val devices = process.inputStream.bufferedReader().useLines { lines ->
                 lines.drop(1) // Skip the header "List of devices attached"
-                    .filter { it.contains("device") && !it.contains("unauthorized") }
+                    .filter { it.contains("device") && !it.contains("unauthorized") && !it.contains("emulator") }
                     .map { line ->
-                        return@map line.split(" ").firstOrNull { it.isNotBlank() } ?: ""
+                        return@map line.split(" ").firstOrNull { it.isNotBlank() }.orEmpty()
                     }.filter { it.isNotBlank() }.toList()
             }
             LOGGER.info("Adb devices: $devices")
--- server/src/main/kotlin/com/unde/server/constants/Time.kt
+++ server/src/main/kotlin/com/unde/server/constants/Time.kt
@@ -4,5 +4,4 @@ import kotlin.time.Duration.Companion.seconds
 
 internal object Time {
     val DEFAULT_ADB_TASK_DELAY = 10L.seconds.inWholeMilliseconds
-    const val INITIAL_ADB_TASK_DELAY = 0L
 }
