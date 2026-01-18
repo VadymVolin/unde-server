560107da3df5da2a24282d84acb7493e24815241
Vadym Volin <vadim.volin1@gmail.com>
2026-01-18 14:03:53 +0200

    plain socket migration

--- server/src/main/kotlin/com/unde/server/socket/remote/ServerSocketConnection.kt
+++ server/src/main/kotlin/com/unde/server/socket/remote/ServerSocketConnection.kt
@@ -7,9 +7,9 @@ import io.ktor.network.selector.*
 import io.ktor.network.sockets.*
 import io.ktor.util.logging.*
 import io.ktor.utils.io.*
-import io.ktor.websocket.readText
 import kotlinx.coroutines.*
 import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.serialization.json.Json
 
 internal object ServerSocketConnection {
@@ -21,12 +21,12 @@ internal object ServerSocketConnection {
     }
 
     private val selectorManager = SelectorManager(Dispatchers.IO)
-    private var socketScope: CoroutineScope? = CoroutineScope(Dispatchers.IO + SupervisorJob())
+    private var socketScope: CoroutineScope? = null
     private var serverSocket: ServerSocket? = null
     private val sockets = mutableMapOf<String, Socket>()
 
     internal fun connect(host: String, port: Int) {
-        val scope = socketScope ?: CoroutineScope(Dispatchers.IO).also { socketScope = it }
+        val scope = socketScope ?: CoroutineScope(Dispatchers.IO + SupervisorJob()).also { socketScope = it }
         scope.launch { // server coroutine
             logger.info("Connecting server socket to $host:$port")
             serverSocket = aSocket(selectorManager).tcp().bind(host, port).also { ss ->
@@ -36,7 +36,10 @@ internal object ServerSocketConnection {
                     try {
                         with(ss.accept()) {
                             socketId = remoteAddress.toString()
-                            connectClientById(socketId, this)
+                            sockets[socketId] = this
+                            WSDataManager.addRemoteConnection(socketId)
+                            logger.info("Accepted connection to client socket: $socketId")
+                            listenSocketById(socketId, this)
                         }
                     } catch (e: Exception) {
                         if (e is CancellationException) throw e
@@ -49,32 +52,39 @@ internal object ServerSocketConnection {
     }
 
     internal fun disconnect() {
-        // todo: release sockets map here
-        serverSocket?.close()
-        socketScope?.cancel()
+        sockets.keys.forEach {
+            disconnectClientById(it)
+        }
+        sockets.clear()
+        try {
+            socketScope?.cancel()
+            serverSocket?.dispose()
+        } catch (e: Exception) {
+            logger.error("Cannot release server socket")
+        }
     }
 
-    private fun connectClientById(clientId: String, client: Socket) {
-        sockets[clientId] = client
-        logger.info("Accepted connection to client socket: $clientId")
+    private fun listenSocketById(clientId: String, client: Socket) {
         client.launch {
-            // todo: extract it to a separate method and add try catch handling
             val readChannel = client.openReadChannel()
             val writeChannel = client.openWriteChannel(true)
             while (!client.isClosed && !readChannel.isClosedForRead) {
+                logger.info("Listen for a new message: ")
                 try {
-                    val byteArray = ByteArray(readChannel.readUTF8Line()?.toInt() ?: error("Frame size wasn't specified"))
+                    val byteArray = ByteArray(readChannel.readUTF8Line()!!.toInt())
                     ensureActive()
                     readChannel.readFully(byteArray)
                     ensureActive()
                     val data = byteArray.decodeToString()
                     handleReceivedData(clientId, data)
                 } catch (e: Exception) {
-                    if (e is CancellationException) throw e
-                    logger.warn("Failed to read data from client $clientId, data is null, start disconnection")
-                    // todo: check how we disconnect from the client, exception is not the best scenario for this case, maybe custom one, or simply track null line
-                    disconnectClientById(clientId)
-                    break
+                    when (e) {
+                        is CancellationException -> throw e
+                        else -> {
+                            logger.error("Failed to read data from client $clientId, data is null, start disconnection")
+                            disconnectClientById(clientId)
+                        }
+                    }
                 }
             }
         }
@@ -84,7 +94,6 @@ internal object ServerSocketConnection {
 
     private fun handleReceivedData(clientId: String, data: String) {
         try {
-            logger.warn("Received data from client $clientId: $data")
             val message = json.decodeFromString<SocketRemoteMessage>(data)
             logger.info("Message from client[$clientId] has been received: $data")
             when (message) {
@@ -113,15 +122,20 @@ internal object ServerSocketConnection {
                 }
             }
         } catch (e: Exception) {
-            logger.error("Failed to parse message: ${e.message}")
+            logger.error("Failed to parse message: ", e)
         }
     }
 
     private fun disconnectClientById(socketId: String) {
         with(sockets.remove(socketId) ?: return) {
-            cancel()
-            dispose()
-            logger.info("Client socket disconnected")
+            try {
+                WSDataManager.removeRemoteConnection(socketId)
+                cancel()
+                dispose()
+                logger.info("Client socket[$socketId] disconnected")
+            } catch (e: Exception) {
+                logger.error("Cannot disconnect socket[$socketId]", e)
+            }
         }
     }
 }
--- server/src/main/kotlin/com/unde/server/socket/remote/WSRemoteConnection.kt
+++ server/src/main/kotlin/com/unde/server/socket/remote/WSRemoteConnection.kt
@@ -13,75 +13,75 @@ import kotlinx.coroutines.isActive
 import kotlinx.serialization.json.Json
 import kotlin.coroutines.cancellation.CancellationException
 
-internal class WSRemoteConnection(
-    val connectionId: String,
-    private val session: DefaultWebSocketServerSession
-)  {
-    private val logger = KtorSimpleLogger(javaClass.simpleName)
-
-    private val json = Json { classDiscriminator = JsonToken.TYPE_TOKEN }
-
-    internal suspend fun connect() {
-        logger.info("The connection to device[$connectionId] has been established")
-        try {
-            WSDataManager.addRemoteConnection(connectionId)
-            send(SocketRemoteMessage.Command(createResultCommandData(Text.CONNECTED_SUCCESSFULLY)))
-            session.incoming.consumeEach(::handleMessage)
-        } catch (e: Exception) {
-            if (e is CancellationException) throw e
-            logger.info("Exception has been caught, device[$connectionId]: $e")
-            WSDataManager.removeRemoteConnection(connectionId)
-        } finally {
-            logger.info("Device[$connectionId] has been disconnected")
-            WSDataManager.removeRemoteConnection(connectionId)
-        }
-    }
-
-    internal suspend fun send(message: SocketRemoteMessage) {
-        try {
-            if (session.isActive) {
-                session.send(Frame.Text(json.encodeToString(SocketRemoteMessage.serializer(), message)))
-            }
-        } catch (e: Exception) {
-            if (e is CancellationException) throw e
-            logger.error("Cannot send message to device[$connectionId]: ${e.message}")
-        }
-    }
-
-    private fun handleMessage(frame: Frame) {
-        if (frame is Frame.Text) {
-            try {
-                val text = frame.readText()
-                val message = json.decodeFromString<SocketRemoteMessage>(text)
-                logger.info("Message from device[$connectionId] has been received: $text")
-                when (message) {
-                    is SocketRemoteMessage.Command -> {
-                        logger.info("Received COMMAND message: ${message.data}")
-                    }
-
-                    is SocketRemoteMessage.Network -> {
-                        logger.info("Received NETWORK message: ${message.data}")
-                        WSDataManager.addNetworkMessage(connectionId, message)
-                    }
-
-                    is SocketRemoteMessage.Telemetry -> {
-                        logger.info("Received TELEMETRY message: ${message.data}")
-//                        WSDataManager.addTelemetry(remoteId, message)
-                    }
-
-                    is SocketRemoteMessage.Logcat -> {
-                        logger.info("Received LOGCAT message: ${message.data}")
-//                        WSDataManager.addLogcatTrace(remoteId, message)
-                    }
-
-                    is SocketRemoteMessage.Database -> {
-                        logger.info("Received DATABASE message: ${message.data}")
-//                        WSDataManager.addDatabaseTrace(remoteId, message)
-                    }
-                }
-            } catch (e: Exception) {
-                logger.error("Failed to parse WSLibraryMessage: ${e.message}")
-            }
-        }
-    }
-}
+//internal class WSRemoteConnection(
+//    val connectionId: String,
+//    private val session: DefaultWebSocketServerSession
+//)  {
+//    private val logger = KtorSimpleLogger(javaClass.simpleName)
+//
+//    private val json = Json { classDiscriminator = JsonToken.TYPE_TOKEN }
+//
+//    internal suspend fun connect() {
+//        logger.info("The connection to device[$connectionId] has been established")
+//        try {
+//            WSDataManager.addRemoteConnection(connectionId)
+//            send(SocketRemoteMessage.Command(createResultCommandData(Text.CONNECTED_SUCCESSFULLY)))
+//            session.incoming.consumeEach(::handleMessage)
+//        } catch (e: Exception) {
+//            if (e is CancellationException) throw e
+//            logger.info("Exception has been caught, device[$connectionId]: $e")
+//            WSDataManager.removeRemoteConnection(connectionId)
+//        } finally {
+//            logger.info("Device[$connectionId] has been disconnected")
+//            WSDataManager.removeRemoteConnection(connectionId)
+//        }
+//    }
+//
+//    internal suspend fun send(message: SocketRemoteMessage) {
+//        try {
+//            if (session.isActive) {
+//                session.send(Frame.Text(json.encodeToString(SocketRemoteMessage.serializer(), message)))
+//            }
+//        } catch (e: Exception) {
+//            if (e is CancellationException) throw e
+//            logger.error("Cannot send message to device[$connectionId]: ${e.message}")
+//        }
+//    }
+//
+//    private fun handleMessage(frame: Frame) {
+//        if (frame is Frame.Text) {
+//            try {
+//                val text = frame.readText()
+//                val message = json.decodeFromString<SocketRemoteMessage>(text)
+//                logger.info("Message from device[$connectionId] has been received: $text")
+//                when (message) {
+//                    is SocketRemoteMessage.Command -> {
+//                        logger.info("Received COMMAND message: ${message.data}")
+//                    }
+//
+//                    is SocketRemoteMessage.Network -> {
+//                        logger.info("Received NETWORK message: ${message.data}")
+//                        WSDataManager.addNetworkMessage(connectionId, message)
+//                    }
+//
+//                    is SocketRemoteMessage.Telemetry -> {
+//                        logger.info("Received TELEMETRY message: ${message.data}")
+////                        WSDataManager.addTelemetry(remoteId, message)
+//                    }
+//
+//                    is SocketRemoteMessage.Logcat -> {
+//                        logger.info("Received LOGCAT message: ${message.data}")
+////                        WSDataManager.addLogcatTrace(remoteId, message)
+//                    }
+//
+//                    is SocketRemoteMessage.Database -> {
+//                        logger.info("Received DATABASE message: ${message.data}")
+////                        WSDataManager.addDatabaseTrace(remoteId, message)
+//                    }
+//                }
+//            } catch (e: Exception) {
+//                logger.error("Failed to parse WSLibraryMessage: ${e.message}")
+//            }
+//        }
+//    }
+//}
